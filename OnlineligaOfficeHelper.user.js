c89526b222d0e8da4b3e7b09e78790cbb2a3f234
Willem Weber <w.weber@supgroup365.de>
2021-08-12 07:25:52 +0200

    minor adjustments

--- OnlineligaOfficeHelper.user.js
+++ OnlineligaOfficeHelper.user.js
@@ -9,6 +9,8 @@
 // @description    Helper for office of onlineliga.*
 // @author         TobSob / KnutEdelbert
 // @match          https://www.onlineliga.de
+// @match          https://www.onlineliga.at
+// @match          https://www.onlineliga.ch
 // @require        https://greasyfork.org/scripts/424896-olcore/code/OLCore.user.js
 // @grant          GM_listValues
 // @grant          GM_setValue
@@ -27,31 +29,31 @@
 
     const Sponsor = {};
     const sponsors = {};
-    var seasonValueMin = Number.MAX_VALUE;
-    var seasonValueMax = 0;
-    var seasonValuePromotedMin = Number.MAX_VALUE;
-    var seasonValuePromotedMax = 0;
+    let seasonValueMin = Number.MAX_VALUE;
+    let seasonValueMax = 0;
+    let seasonValuePromotedMin = Number.MAX_VALUE;
+    let seasonValuePromotedMax = 0;
+
     /* @author: TobSob */
     Sponsor.calculateSponsorsBonuses = function() {
         seasonValueMin = Number.MAX_VALUE;
         seasonValueMax = 0;
-        var i = 0;
         $('.sponsor-seasonvalue').remove();
-        $('div.sponsor-info-details-wrapper').each(function() {
+        $('div.sponsor-info-details-wrapper').each(function(i, el) {
             const valueDict = {}
-            $(this).find('div.sponsor-bonus-value').each(function() {
-                valueDict[$(this).next("div.sponsor-bonus-category").html()] = $(this).html();
+            $(el).find('div.sponsor-bonus-value').each(function(i2, sbv) {
+                valueDict[$(sbv).next("div.sponsor-bonus-category").html()] = $(sbv).html();
             });
-            var seasonValue = 0;
-            var seasonValuePromoted = 0;
-            for(var key in valueDict){
-                var valueString = valueDict[key];
+            let seasonValue = 0;
+            let seasonValuePromoted = 0;
+            for(const key in valueDict){
+                let valueString = valueDict[key];
                 valueString = valueString.replace(/ |,|\.|€|CHF/g,"");
                 const value = parseInt(valueString);
                 const victories = parseInt($('#calcsponsor_victories').val());
                 const placement = parseInt($('#calcsponsor_placement').val());
-                GM_setValue( "victories", victories )
-                GM_setValue( "placement", placement )
+                GM_setValue( "CalcSponsor_victories", victories )
+                GM_setValue( "CalcSponsor_placement", placement )
                 if(key === "Antrittsprämie") {
                     seasonValue += value*34;
                 }
@@ -72,37 +74,36 @@
                 }
             }
             seasonValuePromoted += seasonValue;
-            var paragraphString = OLCore.num2Cur(seasonValue);
+            let paragraphString = OLCore.num2Cur(seasonValue);
             if(seasonValuePromoted !== seasonValue && !isNaN(seasonValuePromoted)){
                 paragraphString += '</span> / <span id="sponsor-seasonvaluepromoted-'+i+'" style="width: fit-content">'+OLCore.num2Cur(seasonValuePromoted)+'*';
             }
-            $(this).after('<div class="sponsor-seasonvalue" style="bottom: 25px; position: relative; left: 5px; width: fit-content">Prämie-Saison: <span id="sponsor-seasonvalue-'+i+'" style="width: fit-content">'+ paragraphString +'</span>');
+            $(el).after('<div class="sponsor-seasonvalue" style="bottom: 25px; position: relative; left: 5px; width: fit-content">Prämie-Saison: <span id="sponsor-seasonvalue-'+i+'" style="width: fit-content">'+ paragraphString +'</span>');
             sponsors[i] = [seasonValue, seasonValuePromoted];
-            i = i+1;
             seasonValueMax = Math.max(seasonValueMax, seasonValue);
             seasonValueMin = Math.min(seasonValueMin, seasonValue);
             seasonValuePromotedMax = Math.max(seasonValuePromotedMax, seasonValuePromoted);
             seasonValuePromotedMin = Math.min(seasonValuePromotedMin, seasonValuePromoted);
+            for(const key in sponsors) {
+                let value = (sponsors[key][0] - seasonValueMin) / seasonValueMax;
+                $("#sponsor-seasonvalue-"+key).css('background-color',getColor(value));
+                value = (sponsors[key][1] - seasonValuePromotedMin) / seasonValuePromotedMax;
+                $("#sponsor-seasonvaluepromoted-"+key).css('background-color',getColor(value));
+            }
         });
-        for(var key in sponsors) {
-            var value = (sponsors[key][0] - seasonValueMin) / seasonValueMax;
-            $("#sponsor-seasonvalue-"+key).css('background-color',getColor(value));
-            value = (sponsors[key][1] - seasonValuePromotedMin) / seasonValuePromotedMax;
-            $("#sponsor-seasonvaluepromoted-"+key).css('background-color',getColor(value));
-        }
     };
 
     function getColor(value){
-        var hue=((value)*120).toString(10);
+        const hue=((value)*120).toString(10);
         return ["hsla(",hue,",100%,50%,0.4)"].join("");
     }
 
     /* @author: TobSob */
     function generateSponsorCalcDiv() {
 
-        $("li#filter-base a,li#filter-promotion a,li#filter-champion a,li#filter-remain a,li#filter-placement a,li#filter-win a").bind( "click", function() { OLCore.waitForKeyElements ( "div.sponsor-pool-row", Sponsor.calculateSponsorsBonuses,);});
-        const initVict = GM_getValue( "victories", 10 )
-        const initPlacem = GM_getValue( "placement", 10 )
+        //$("li#filter-base a,li#filter-promotion a,li#filter-champion a,li#filter-remain a,li#filter-placement a,li#filter-win a").bind( "click", function() { OLCore.waitForKeyElements ( "div.sponsor-pool-row", Sponsor.calculateSponsorsBonuses,);});
+        const initVict = GM_getValue( "CalcSponsor_victories", 10 );
+        const initPlacem = GM_getValue( "CalcSponsor_placement", 10 );
         const cl = OLCore.Base.teamColorNumber;
         $('#ol-root').append(`<div style="background-color: transparent;" class="sponsor-calc-div">
         <span class="sponsor-calc-input ol-state-primary-text-color-${cl}">Siege</span> \n<input class="sponsor-calc-input ol-state-primary-text-color-${cl}" placeholder="Siege" type="number" max="34" min="0" id="calcsponsor_victories" value="`+initVict+`"/></br>
@@ -115,12 +116,17 @@
         if (myBtn) {
             myBtn.addEventListener ("click", Sponsor.calculateSponsorsBonuses , false);
         }
+
+        OLCore.waitForKeyElements (
+            "div.sponsor-pool-row",
+            Sponsor.calculateSponsorsBonuses
+        );
     }
 
     function init(){
         OLCore.waitForKeyElements (
             "div#sponsorContainer",
-            generateSponsorCalcDiv,
+            generateSponsorCalcDiv
         );
         GM_addStyle('* .sponsor-calc-div { position: fixed; bottom:50%;right: 20px; text-decoration: none; color: #000000;background-color: rgba(235, 235, 235, 0.80);font-size: 12px;padding: 1em;} .sponsor-calc-input { width:100%; margin: 5px }');
     }
